019c05c547143e1e0dcebdff9730e6eca1ada856
ooplata <99055524+ooplata@users.noreply.github.com>
2022-10-20 08:55:44 -0500

    Switched to edges and vertices for MapPanel, added connexions

--- src/ui/MapPanel.java
+++ src/ui/MapPanel.java
@@ -1,31 +1,34 @@
 package ui;
 
+import graphing.Edge;
+import graphing.Vertex;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class MapPanel extends JPanel {
-    private final HashSet<Point> points = new HashSet<>();
-    private Point prev = null;
+    private final ArrayList<Vertex> vertices = new ArrayList<>();
+    private Vertex prev;
 
     public MapPanel() {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                var point = new Point(e.getX(), e.getY());
-                if (!points.contains(point)) {
-                    points.add(point);
-                    repaint();
+                int x = e.getX();
+                int y = e.getY();
 
-                    if (prev != null) {
-                        //TODO: ask user for the cost, draw line
-                        prev = null;
-                    } else {
-                        prev = point;
-                    }
-                }
+                if (!canAdd(x, y)) return;
+
+                var vertex = new Vertex(x, y);
+                vertices.add(vertex);
+
+                if (prev != null) prev.edges.add(new Edge(vertex, 1));
+                prev = vertex;
+
+                repaint();
             }
         });
     }
@@ -33,12 +36,27 @@ public class MapPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.RED);
 
         var graphics2D = (Graphics2D) g;
-        graphics2D.setColor(Color.RED);
-
-        for (Point p : points) {
-            graphics2D.fillOval(p.x, p.y, 5, 5);
+        for (var vertex : vertices) {
+            graphics2D.fillOval(vertex.x, vertex.y, 8, 8);
+            for (var v : vertex.edges) {
+                g.drawLine(v.destination().x + 4, v.destination().y + 4, vertex.x + 4, vertex.y + 4);
+            }
         }
+    }
+
+    private boolean canAdd(int x, int y) {
+        for (int xOffset = -8; xOffset <= 8; xOffset++) {
+            for (int yOffset = -8; yOffset <= 8; yOffset++) {
+                var vertex = new Vertex(x + xOffset, y + yOffset);
+                if (vertices.contains(vertex)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
