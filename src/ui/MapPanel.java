bf5c1883078afe2fe824cce1ad39d1233c39d317
ooplata <99055524+ooplata@users.noreply.github.com>
2022-10-24 10:52:27 -0500

    Switched to solar system items in MapPanel

--- src/ui/MapPanel.java
+++ src/ui/MapPanel.java
@@ -1,8 +1,8 @@
 package ui;
 
 import assets.Resources;
-import graphing.Edge;
-import graphing.Vertex;
+import graphing.SolarSystemHelper;
+import graphing.SolarSystemItem;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -14,9 +14,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MapPanel extends JPanel {
-    private final ArrayList<Vertex> vertices = new ArrayList<>();
-    private Vertex selected;
-    private Vertex prev;
+    private final ArrayList<SolarSystemItem> items = new ArrayList<>();
+    private SolarSystemItem selected;
 
     private BufferedImage bg;
 
@@ -30,51 +29,60 @@ public class MapPanel extends JPanel {
     }
 
     private void onClick(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-
-        if (selected != null) {
-            var curr = vertexAt(x, y);
-            if (curr == null) {
-                addVertex(selected, x, y);
+        var point = e.getPoint();
+        if (selected == null) {
+            if (items.isEmpty()) {
+                selected = addItem(null, point);
             } else {
-                if (!curr.parent.equals(selected) || !selected.parent.equals(curr)) {
-                    var edge = new Edge(selected, 1);
-                    curr.edges.add(edge);
-                    System.out.println("added");
-                }
+                selected = itemAt(point);
             }
-
-            selected = null;
         } else {
-            selected = vertexAt(x, y);
-            if (selected == null) prev = addVertex(prev, x, y);
+            selected = addItem(selected, point);
         }
 
         repaint();
     }
 
-    private Vertex vertexAt(int x, int y) {
-        for (int xOffset = -8; xOffset <= 8; xOffset++) {
-            for (int yOffset = -8; yOffset <= 8; yOffset++) {
-                var vertex = new Vertex(null, x + xOffset, y + yOffset);
-                int index = vertices.indexOf(vertex);
+    private SolarSystemItem addItem(SolarSystemItem parent, Point bounds) {
+        int index = getItemIndexFromPoint(bounds);
+        if (index == -1) return null;
 
-                if (index >= 0) return vertices.get(index);
+        var item = new SolarSystemItem(index);
+        int listIndex = items.indexOf(item);
+
+        if (listIndex != -1) {
+            item = items.get(listIndex);
+        } else {
+            items.add(item);
+        }
+
+        if (parent != null) {
+            parent.addConnection(item);
+        }
+        return item;
+    }
+
+    private SolarSystemItem itemAt(Point point) {
+        int index = getItemIndexFromPoint(point);
+        if (index != -1) {
+            var item = new SolarSystemItem(index);
+            int itemIndex = items.indexOf(item);
+
+            if (itemIndex != -1) {
+                return items.get(itemIndex);
             }
         }
         return null;
     }
 
-    private Vertex addVertex(Vertex parent, int x, int y) {
-        var vertex = new Vertex(parent, x, y);
-        vertices.add(vertex);
-
-        if (parent != null) {
-            var edge = new Edge(vertex, 1);
-            parent.edges.add(edge);
+    private int getItemIndexFromPoint(Point point) {
+        for (int i = 0; i < 10; i++) {
+            if (SolarSystemHelper.getItemBounds(i).contains(point)) {
+                return i;
+            }
         }
-        return vertex;
+
+        return -1;
     }
 
     @Override
@@ -85,18 +93,21 @@ public class MapPanel extends JPanel {
         g.setColor(Color.RED);
 
         var graphics2D = (Graphics2D) g;
-        for (var vertex : vertices) {
-            graphics2D.fillOval(vertex.x, vertex.y, 8, 8);
-            for (var v : vertex.edges) {
-                g.drawLine(v.destination().x + 4, v.destination().y + 4, vertex.x + 4, vertex.y + 4);
+        for (var vertex : items) {
+            int centerX = (int) vertex.getBounds().getCenterX();
+            int centerY = (int) vertex.getBounds().getCenterY();
+            graphics2D.fillOval(centerX, centerY, 8, 8);
+
+            for (var v : vertex.connections) {
+                int connCenterX = (int) v.getBounds().getCenterX();
+                int connCenterY = (int) v.getBounds().getCenterY();
+                g.drawLine(connCenterX + 4, connCenterY + 4, centerX + 4, centerY + 4);
             }
         }
 
         g.setColor(Color.BLUE);
         if (selected != null) {
-            drawSquare(g, selected.x, selected.y);
-        } else if (prev != null) {
-            drawSquare(g, prev.x, prev.y);
+            drawSquare(g, (int) selected.getBounds().getCenterX(), (int) selected.getBounds().getCenterY());
         }
     }
 
