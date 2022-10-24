0d6106a54f5cfa64e027d7dda57bdab90716648c
ooplata <99055524+ooplata@users.noreply.github.com>
2022-10-24 10:46:40 -0500

    Added bounds support to solar system helper

--- src/graphing/SolarSystemHelper.java
+++ src/graphing/SolarSystemHelper.java
@@ -2,26 +2,51 @@ package graphing;
 
 import assets.Resources;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public final class SolarSystemHelper {
-    private static volatile String[] lines;
+    private static Object[] bounds;
+    private static Object[] distances;
+
+    public static Rectangle getItemBounds(int itemIndex) {
+        if (bounds == null) getBounds();
+
+        var split = bounds[itemIndex].toString().split(",");
+
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        int width = Integer.parseInt(split[2]);
+        int height = Integer.parseInt(split[3]);
+
+        return new Rectangle(x, y, width, height);
+    }
 
     public static long getDistanceBetweenItems(int startIndex, int destinationIndex) {
-        if (lines == null) getLines();
+        if (distances == null) getDistances();
 
-        var split = lines[startIndex].split(",");
+        var split = distances[startIndex].toString().split(",");
         return Long.parseLong(split[destinationIndex]);
     }
 
-    private static void getLines() {
+    private static void getBounds() {
+        try (var stream = Resources.getResourceAsStream("PlanetBounds.txt")) {
+            assert stream != null;
+
+            var streamReader = new BufferedReader(new InputStreamReader(stream));
+            bounds = streamReader.lines().toArray();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private static void getDistances() {
         try (var stream = Resources.getResourceAsStream("Planets.txt")) {
             assert stream != null;
 
             var streamReader = new BufferedReader(new InputStreamReader(stream));
-            lines = (String[]) streamReader.lines().toArray();
+            distances = streamReader.lines().toArray();
         } catch (IOException ignored) {
         }
     }
